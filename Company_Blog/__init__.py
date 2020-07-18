80a9603cbb93068b3c3a0803fad674905fb40c10
Rohit Dubey <dubey22rohit@gmail.com>
2020-07-18 23:33:31 +0530

    Stage 1 Complete

--- Company_Blog/__init__.py
+++ Company_Blog/__init__.py
@@ -1,3 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import os
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+from Company_Blog.core.views import core
+from Company_Blog.error_pages.handlers import error_pages
+from Company_Blog.users.views import users
+app.register_blueprint(error_pages)
+app.register_blueprint(core)
+app.register_blueprint(users)
---
+++ Company_Blog/users/forms.py
@@ -0,0 +1,49 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+from flask_wtf.file import FileField, FileAllowed
+
+from flask_login import current_user
+from Company_Blog.models import User
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo(
+        'pass_confirm', message='Passwords must match')])
+    pass_confirm = PasswordField(
+        'Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def check_email(self, field):
+        if User.quesr.filter_by(email=field.data).first():
+            raise ValidationError(
+                message='Your Email has already been registered')
+
+    def check_username(self, field):
+        if User.quesr.filter_by(username=field.data).first():
+            raise ValidationError(
+                message='Your Username has already been registered')
+
+
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    picture = FileField('Update Profile Picture', FileAllowed=(['jpg', 'png']))
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.quesr.filter_by(email=field.data).first():
+            raise ValidationError(
+                message='Your Email has already been registered')
+
+    def check_username(self, field):
+        if User.quesr.filter_by(username=field.data).first():
+            raise ValidationError(
+                message='Your Username has already been registered')
